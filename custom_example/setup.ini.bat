4a4b5744a5cd4db390bb9cc52b91f3f7cb036e74
VonC <vonc@laposte.net>
2019-06-13 23:34:00 +0200

    Add custom_example

---
+++ custom_example/setup.ini.bat
@@ -0,0 +1,10 @@
+@echo off
+REM local  folder (on a local drive)
+REM where programs archives are uncompressed and programs installed. 
+set PRGS=
+REM local or (better) remote folder (on a network drive)
+REM where config files are keps. 
+set HOME=
+REM local folder (on a local drive)
+REM where Git repositories and developments are done. 
+set PROG=
