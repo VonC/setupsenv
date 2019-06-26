703b53d9b20edbcf3c8115807098a866b355d61e
VonC <vonc@laposte.net>
2019-06-26 13:22:39 +0200

    setup.bat generates senv/gsenv

--- setup.bat
+++ setup.bat
@@ -28,8 +28,8 @@ if "%PRGS%"=="" ( %_fatal% "PRGS (installation folder) must be defined in custom
 if "%HOME%"=="" ( %_fatal% "HOME must be defined in custom/setup.ini.bat" && exit /b 1 )
 if "%PROG%"=="" ( %_fatal% "PROG (installation folder) must be defined in custom/setup.ini.bat" && exit /b 1 )
 
-echo @echo off%NL%call %HOME%\bin\senv.bat> "%USERPROFILE%\senv2.bat"
-echo @echo off%NL%call %HOME%\bin\gsenv.bat> "%USERPROFILE%\gsenv2.bat"
+echo @echo off%NL%call %HOME%\bin\senv.bat> "%USERPROFILE%\senv.bat"
+echo @echo off%NL%call %HOME%\bin\gsenv.bat> "%USERPROFILE%\gsenv.bat"
 doskey senv=
 
 %_info% "PRGS='%PRGS%'"
