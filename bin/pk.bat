fe9fecdaf445ac9a98ff0ffd63e60d30b27235d3
VonC <vonc@laposte.net>
2019-08-21 11:24:28 +0200

    pk.bat, add grep -a and CRLF

--- bin/pk.bat
+++ bin/pk.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-for /f "tokens=1,2" %%a in ('tasklist^|grep -i %1') do ( set pname=%%a&& set pid=%%b )
+for /f "tokens=1,2" %%a in ('tasklist^|grep -ai %1') do ( set pname=%%a&& set pid=%%b )
 echo "pname='%pname%' => pid='%pid%'
 
 if not "%pid%"=="" (
