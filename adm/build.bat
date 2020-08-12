cc8b2e946cb3748caa29002f3a00131377f047d0
Vonc <vonc@laposte.net>
2020-08-12 17:36:29 +0200

    build is the new bundle

---
+++ adm/build.bat
@@ -0,0 +1,46 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
+cd /d "%script_dir%" || echo "unable to cd to '%script_dir%'"&& exit /b 1
+
+set "bc=%script_dir%\..\batcolors"
+call "%bc%\echos_macros.bat"
+%_info% "script_dir(bundle)='%script_dir%'"
+
+if "%1" == "" (
+    %_fatal% "Must have setupsdir profile xx, for calling setupsdir_xx.bat" 1
+)
+
+set s="setupsdir_%1.bat"
+if not exist "%script_dir%\%s%" (
+    %_fatal% "setupsdir script '%s%' does not exist" 2
+)
+
+copy /Y "%script_dir%\%s%" "%script_dir%\setupsdir.bat"
+copy /Y "%script_dir%\install_%1.list" "%script_dir%\install.list"
+
+git config --unset user.name
+git config --unset user.email
+
+cd ..
+if not exist custom (
+    %_fatal% "current folder must be named custom" 1
+)
+git config --unset user.name
+git config --unset user.email
+cd ..
+if not exist senv (
+    %_fatal% "custom must be in senv folder" 1
+)
+del senv_%1.zip
+del senv_%1.zip.exe
+del senv_%1-zip.exe
+%_info% "zip '%script_dir%\..\..\senv' to 'senv_%1.zip'"
+%sz% a -sfx7z.sfx senv_%1-zip.exe senv
+cd senv
+call gcuvc
+cd custom
+call gcu
+
+git restore install.list
