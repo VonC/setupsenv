7b00a0b001aebd22207d2605b5478cf39168fc54
Vonc <vonc@laposte.net>
2021-03-04 16:02:39 +0100

    build_all.bat: rebuild setupsenv for all profiles

---
+++ adm/build_all.bat
@@ -0,0 +1,48 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
+cd /d "%script_dir%" || echo "unable to cd to '%script_dir%'"&& exit /b 1
+
+set "bc=%script_dir%\..\batcolors"
+call "%bc%\echos_macros.bat"
+%_info% "script_dir(publish)='%script_dir%'"
+
+cd ../custom || %_fatal% "Unable to access custom folder" 1
+for /F "delims=" %%f in ('pwd') do ( set cpwd=%%f )
+%_info% "Custom folder full path: '%cpwd%'"
+
+call:execcmd "ls -1 setupsdir*_*|cut -d _ -f 2|cut -d . -f 1"
+for /L %%n in (1 1 !output_cnt!) DO (
+    rem %_info% "profile exec(%%n)='!output[%%n]!'"
+    set "profiles[%%n]=!output[%%n]!"
+    rem %_info% "profile stored(%%n)='!profiles[%%n]!'"
+)
+
+for /L %%n in (1 1 !output_cnt!) DO (
+    set "profile=!profiles[%%n]!"
+    %_info% "profile='!profile!'"
+    call build.bat !profile!
+)
+goto:eof
+
+:execcmd
+rem echo aaa%~1
+%~1 >a
+set LF=^
+
+
+REM The two empty lines are required here
+rem ls -1 s*_*|xargs grep setupsdir|grep -i HLD|cut -d = -f 2|cut -d'^"' -f 1 | grep setups>a
+rem https://stackoverflow.com/a/31046373/6309
+rem https://stackoverflow.com/questions/31035636/batch-store-command-output-to-a-variable-multiple-lines
+set "output_cnt=0"
+for /F "delims=" %%f in (a) do (
+    set /a output_cnt+=1
+    set "output[!output_cnt!]=%%f"
+)
+del a
+if errorlevel 1 (
+    pwd
+    %_fatal% "unable to delete a" 2
+)
