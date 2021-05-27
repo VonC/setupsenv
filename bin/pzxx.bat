5f389c14bb0c3bc4f523c2edf734eb2a3dbf9f00
VonC <vonc@laposte.net>
2021-05-27 14:46:13 +0200

    pzxx.bat: support archive names with spaces

--- bin/pzxx.bat
+++ bin/pzxx.bat
@@ -1,3 +1,3 @@
 @echo off
 rem https://superuser.com/questions/194659/how-to-disable-the-output-of-7-zip
-for /F %%i in ("%1") do "%sz%" x -aos -pdefault -sccUTF-8 "%%i" -o%%~ni -bso0 -bsp1
+for /F "delims=," %%i in (%1) do "%sz%" x -aos -pdefault -sccUTF-8 "%%i" -o"%%~ni" -bso0 -bsp1
