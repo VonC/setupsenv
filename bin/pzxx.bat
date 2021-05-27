1d19733d8dc44ea785ea2b8e62da9064350a3eca
VonC <vonc@laposte.net>
2021-05-27 18:27:10 +0200

    bin/pzxx.bat: should be silent -- echo off

--- bin/pzxx.bat
+++ bin/pzxx.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 rem https://superuser.com/questions/194659/how-to-disable-the-output-of-7-zip
 for /F "delims=," %%i in ('echo %1') do "%sz%" x -aos -pdefault -sccUTF-8 "%%i" -o"%%~ni" -bso0 -bsp1
 @echo off
