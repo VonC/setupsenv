f466cc11c4482643a5a8aaec71371c35a5041ec4
VonC <vonc@laposte.net>
2022-04-13 08:12:12 +0200

    installs/drive_detection.bat: detect frive letter for UNC path

---
+++ installs/drive_detection.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:detect_drive
+set "driveUNCPath=%1"
+rem https://stackoverflow.com/questions/1964192/removing-double-quotes-from-variables-in-batch-file-creates-problems-with-cmd-en
+set driveUNCPath=%driveUNCPath:"=%
+set "driveUNCPathEsc=%driveUNCPath:\=\\%"
+rem echo driveUNCPath='%driveUNCPath%', driveUNCPathEsc='%driveUNCPathEsc%'
+
+set "driveLetter="
+for /f "tokens=* delims=" %%i in ('net use') do (
+    echo %%i | findstr "%driveUNCPathEsc% " > NUL
+    if not errorlevel 1 (
+        set "dr=%%i"
+        rem echo possible drive='!dr!'
+        rem @echo on
+        for /f "tokens=1,2 delims= " %%j in ('echo !dr!') do (
+            rem cho drletter='%%j'
+            rem echo 'drpath ='%%k'
+            if "%%k" == "%driveUNCPath%" (
+                set "driveLetter=%%j"
+                goto:found
+            )
+        )
+        rem @echo off
+    )
+)
+:found
+%_info% "Drive found for '%driveUNCPath%': '%driveLetter%'"
+endlocal & set driveLetter=%driveLetter%
