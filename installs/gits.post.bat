385dcd650c031f7f0dc0793eb0e691327031ea9d
VonC <vonc@laposte.net>
2022-05-10 17:07:28 +0200

    Detect incomplete home bin git config: delete and re-init

--- installs/gits.post.bat
+++ installs/gits.post.bat
@@ -50,7 +50,22 @@ if not "%prgtoinstall%"=="" (
 )
 
 "%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\.gitconfig" 1>NUL
-if not exist .git ( git init . && call gcu.bat )
+"%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\bin\.git\config" 1>NUL 2>NUL
+if exist .git\config (
+    grep bare .git\config 1>NUL 2>NUL
+    if errorlevel 1 (
+        %_warning% "%HOME%\bin\.git\config incomplete: delete and redo"
+        del /Q .git\config
+        if errorlevel 1 (
+            %_error% "%HOME%\bin\.git\config unable to be deleted"
+        )
+    )
+)
+if not exist .git\config (
+    %_info% "Initialize git repository in %HOME%\bin"
+    "%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\.gitconfig" 1>NUL
+    git init . && call gcu.bat 
+)
 if not exist .gitignore (  copy "%script_dir%\bin\.gitignore" "%HOME%\bin" )
 
 call:check_gitdate
@@ -61,11 +76,13 @@ if exist "%HOME%\bin\.git\index.lock" (%_fatal% "%HOME%\bin used by other Git pr
 %_info% "   [Calling first git status --porcelain in '%HOME%\bin']"
 set st=
 "%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\.gitconfig" 1>NUL
+"%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\bin\.git\config" 1>NUL
 for /f "delims=" %%x in ('git status --porcelain') do set "st=%%x"
 if not "%st%" == "" (
     %_info% "Save local modification of '%HOME%\bin'"
     rem %_fatal% "no local save" 111
     "%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\.gitconfig" 1>NUL
+    "%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\bin\.git\config" 1>NUL
     git add .
     git commit -m "pre-update"
 )
@@ -84,6 +101,7 @@ copy /Y "%script_dir%\custom\bin\*" "%HOME%\bin" > NUL:
 
 %_info% "   [Check 'git config --local user.name' in '%HOME%\bin']"
 "%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\.gitconfig" 1>NUL
+"%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\bin\.git\config" 1>NUL
 git config --local user.name>NUL
 if errorlevel 1 ( call gcu.bat )
 
@@ -93,11 +111,13 @@ if "%nomodif%"=="1" ( goto:skipsecondstatus)
 %_info% "   [Calling Second git status --porcelain in '%HOME%\bin']"
 set st=
 "%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\.gitconfig" 1>NUL
+"%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\bin\.git\config" 1>NUL
 for /f "delims=" %%x in ('git status --porcelain') do set "st=%%x"
 if not "%st%" == "" (
     %_info% "Save new updates of '%HOME%\bin'"
     rem %_fatal% "no new update save" 112
     "%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\.gitconfig" 1>NUL
+    "%PRGS%\gits\current\usr\bin\cat.exe" "%HOME%\bin\.git\config" 1>NUL
     git add --renormalize .
     git commit -m "post-update"
 )
