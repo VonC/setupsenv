c6c3861a8f3bf2515c1028c88494fe766e6c77f7
VonC <vonc@laposte.net>
2023-08-10 19:32:59 +0200

    installs/ideaics.post.bat: check if org.bouncycastle.asn1.allow_unsafe_integer is present in APPDATA\JetBrains\IdeaIC2022.3\idea.properties

---
+++ installs/ideaics.post.bat
@@ -0,0 +1,44 @@
+@echo off
+if "%script_dir%"=="" (
+    setlocal enabledelayedexpansion
+    for %%i in ("%~dp0..") do SET "script_dir=%%~fi"
+    call "!script_dir!\custom\echos_macros.bat"
+)
+%_info% "~~~~~~~~~~~~ IdeaICs post installation ~~~~~~~~~~~~"
+%_task% "Must extract IntelliJ IDEA IC version from %PRGS%\ideaics\current\product-info.json"
+REM extract version from %PRGS%\ideaics\current\product-info.json, line "-Didea.paths.selector=IdeaIC2022.3", version should be '2022.3'
+
+%_task% "Must check if '%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties' exist"
+if exist "%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties" (
+    REM Check if org.bouncycastle.asn1.allow_unsafe_integer if present in the file
+    %_task% "File exist, so must check if it includes org.bouncycastle.asn1.allow_unsafe_integer=true in the file"
+    findstr /r /c:"org.bouncycastle.asn1.allow_unsafe_integer" "%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties" >nul
+    if errorlevel 1 (
+        %_info% "org.bouncycastle.asn1.allow_unsafe_integer is not present in the file"
+        %_task% "Add org.bouncycastle.asn1.allow_unsafe_integer=true in the file"
+        echo "org.bouncycastle.asn1.allow_unsafe_integer=true" >> "%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties"
+        if errorlevel 1 (
+            %_fatal% "Unable to add org.bouncycastle.asn1.allow_unsafe_integer=true in the file" 1
+        ) else (
+            %_ok% "org.bouncycastle.asn1.allow_unsafe_integer=true added in the file"
+        )
+    ) else (
+        %_ok% "org.bouncycastle.asn1.allow_unsafe_integer is already present in the file"
+    )
+) else (
+    %_info% "No file '%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties' found"
+    REM create the parent folder structure, then copy the file there
+    mkdir "%APPDATA%\JetBrains\IdeaIC2022.3"
+    if errorlevel 1 (
+        %_fatal% "Unable to create '%APPDATA%\JetBrains\IdeaIC2022.3' folder" 1
+    ) else (
+        %_ok% "Folder '%APPDATA%\JetBrains\IdeaIC2022.3' created"
+    )
+    %_task% "Copy 'idea.properties' file to '%APPDATA%\JetBrains\IdeaIC2022.3'"
+    copy /y "%script_dir%\custom\idea.properties" "%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties" >nul
+    if errorlevel 1 (
+        %_fatal% "Unable to copy 'idea.properties' file to '%APPDATA%\JetBrains\IdeaIC2022.3'" 1
+    ) else (
+        %_ok% "'idea.properties' file copied to '%APPDATA%\JetBrains\IdeaIC2022.3'"
+    )
+)
