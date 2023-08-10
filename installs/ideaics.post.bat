d524226278e3a4d40d2ccc524b564957299b34db
VonC <vonc@laposte.net>
2023-08-10 19:56:08 +0200

    ideaics.post.bat: extract IDEA IC version

--- installs/ideaics.post.bat
+++ installs/ideaics.post.bat
@@ -7,16 +7,27 @@ if "%script_dir%"=="" (
 %_info% "~~~~~~~~~~~~ IdeaICs post installation ~~~~~~~~~~~~"
 %_task% "Must extract IntelliJ IDEA IC version from %PRGS%\ideaics\current\product-info.json"
 REM extract version from %PRGS%\ideaics\current\product-info.json, line "-Didea.paths.selector=IdeaIC2022.3", version should be '2022.3'
-
-%_task% "Must check if '%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties' exist"
-if exist "%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties" (
+set "version="
+for /f "usebackq tokens=2 delims=C " %%i in (`findstr /r /c:"-Didea.paths.selector=IdeaIC" "%PRGS%\ideaics\current\product-info.json"`) do (
+    set "version=%%i"
+    set "version=!version: =!"
+    set "version=!version:~0,-2!"
+    goto:versionfound
+)
+:versionfound
+if "%version%"=="" (
+    %_fatal% "Unable to extract version from %PRGS%\ideaics\current\product-info.json" 1
+)
+%_info% "IdeaIC version extracted!: %version%"
+%_task% "Must check if %APPDATA%\JetBrains\IdeaIC%version%\idea.properties exist"
+if exist "%APPDATA%\JetBrains\IdeaIC%version%\idea.properties" (
     REM Check if org.bouncycastle.asn1.allow_unsafe_integer if present in the file
     %_task% "File exist, so must check if it includes org.bouncycastle.asn1.allow_unsafe_integer=true in the file"
-    findstr /r /c:"org.bouncycastle.asn1.allow_unsafe_integer" "%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties" >nul
+    findstr /r /c:"org.bouncycastle.asn1.allow_unsafe_integer" "%APPDATA%\JetBrains\IdeaIC%version%\idea.properties" >nul
     if errorlevel 1 (
         %_info% "org.bouncycastle.asn1.allow_unsafe_integer is not present in the file"
         %_task% "Add org.bouncycastle.asn1.allow_unsafe_integer=true in the file"
-        echo "org.bouncycastle.asn1.allow_unsafe_integer=true" >> "%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties"
+        echo "org.bouncycastle.asn1.allow_unsafe_integer=true" >> "%APPDATA%\JetBrains\IdeaIC%version%\idea.properties"
         if errorlevel 1 (
             %_fatal% "Unable to add org.bouncycastle.asn1.allow_unsafe_integer=true in the file" 1
         ) else (
@@ -26,19 +37,19 @@ if exist "%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties" (
         %_ok% "org.bouncycastle.asn1.allow_unsafe_integer is already present in the file"
     )
 ) else (
-    %_info% "No file '%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties' found"
+    %_info% "No file '%APPDATA%\JetBrains\IdeaIC%version%\idea.properties' found"
     REM create the parent folder structure, then copy the file there
-    mkdir "%APPDATA%\JetBrains\IdeaIC2022.3"
+    mkdir "%APPDATA%\JetBrains\IdeaIC%version%"
     if errorlevel 1 (
-        %_fatal% "Unable to create '%APPDATA%\JetBrains\IdeaIC2022.3' folder" 1
+        %_fatal% "Unable to create '%APPDATA%\JetBrains\IdeaIC%version%' folder" 1
     ) else (
-        %_ok% "Folder '%APPDATA%\JetBrains\IdeaIC2022.3' created"
+        %_ok% "Folder '%APPDATA%\JetBrains\IdeaIC%version%' created"
     )
-    %_task% "Copy 'idea.properties' file to '%APPDATA%\JetBrains\IdeaIC2022.3'"
-    copy /y "%script_dir%\custom\idea.properties" "%APPDATA%\JetBrains\IdeaIC2022.3\idea.properties" >nul
+    %_task% "Copy 'idea.properties' file to '%APPDATA%\JetBrains\IdeaIC%version%'"
+    copy /y "%script_dir%\installs\idea.properties" "%APPDATA%\JetBrains\IdeaIC%version%\idea.properties" >nul
     if errorlevel 1 (
-        %_fatal% "Unable to copy 'idea.properties' file to '%APPDATA%\JetBrains\IdeaIC2022.3'" 1
+        %_fatal% "Unable to copy 'idea.properties' file to '%APPDATA%\JetBrains\IdeaIC%version%'" 1
     ) else (
-        %_ok% "'idea.properties' file copied to '%APPDATA%\JetBrains\IdeaIC2022.3'"
+        %_ok% "'idea.properties' file copied to '%APPDATA%\JetBrains\IdeaIC%version%'"
     )
 )
