6b4eb290df060ac3af35ca634b6a30cc0a18ff07
VonC <vonc@laposte.net>
2023-09-25 10:54:58 +0200

    installs/peazips.post.bat: add missing 7z symlink in peazips/current/res

---
+++ installs/peazips.post.bat
@@ -0,0 +1,26 @@
+@echo off
+
+if "%script_dir%"=="" (
+    setlocal enabledelayedexpansion
+    for %%i in ("%~dp0..") do SET "script_dir=%%~fi"
+    call "!script_dir!\custom\echos_macros.bat"
+)
+%_info% "Peazips post install"
+
+if not exist "%PRGS%\peazips\current\res" (
+    %_fatal% "'%PRGS%\peazips\current\res' folder missing"
+)
+
+if not exist "%PRGS%\peazips\current\res\7z" (
+    %_task% "Must add missing symlink 7z in '%PRGS%\peazips\current\res'"
+    mklink /J "%PRGS%\peazips\current\res\7z" "%PRGS%\peazips\current\res\bin\7z"
+    if errorlevel 1 (
+        %_fatal% "unable to add symlink 7z in '%PRGS%\peazips\current\res' to '%PRGS%\peazips\current\res\bin\7z'" 1
+    )
+    %_ok% "symlink 7z created in '%PRGS%\peazips\current\res' to '%PRGS%\peazips\current\res\bin\7z'"
+)
+
+if not exist "%PRGS%\peazips\current\res\7z\7z.exe" (
+    %_fatal% "unable to access 7z.exe in '%PRGS%\peazips\current\res\7z'" 1
+)
+%_ok% " 7z.exe in '%PRGS%\peazips\current\res\7z' is accessible"
