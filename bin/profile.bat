55372bd95908d45271fda3d6cfbdaa62dd81d8f4
VonC <vonc@laposte.net>
2024-07-15 13:56:09 +0200

    feat(bin): profile.bat gives the name of the profile

---
+++ bin/profile.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+FOR /F %%i IN ('cat %HOME%\bin\profile') DO set profileName=%%i
+echo profile name='%profileName%'
+FOR /F "tokens=3,* delims= " %%i IN ('alias cdis') DO set profileFolder=%%i
+set "profileFolder=%profileFolder:~1,-1%"
+echo from profile folder '%profileFolder%'
