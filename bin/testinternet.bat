f7d8fb498a2af95ba3cefc70627d05c768ac2aeb
VonC <vonc@laposte.net>
2024-07-15 13:57:51 +0200

    feat(bin): testinternet.bat check if internet is accessible

---
+++ bin/testinternet.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal enabledelayedexpansion
+rem goto:clean_path
+
+for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
+call %script_dir_bin%\echos_macros.bat
+
+%_task% "Must check HTTP code by querying www.google.com"
+for /f "tokens=*" %%i in ('curl -Lks -o /dev/null -m 3 -w %%{http_code} https://www.google.com') do ( set "code=%%i" )
+if "%code%" == "200" (
+  %_ok% "Internet connection is working"
+  goto:eof
+)
+%_warning% "Code HTTP '%code%', check errorlevel:"
+curl -Lks -o /dev/null -m 3 -w %%{http_code}\n https://www.google.com
+set "err=%ERRORLEVEL%"
+%_error% "Code HTTP '%code%', errorlevel '%err%'"
