363ee159aedbffa73438a365c3f4a3d885fc8044
VonC <vonc@laposte.net>
2024-07-15 16:10:37 +0200

    feat(installs): javas.sln.bat proposes sln jdkx based on jdk installation folder

---
+++ installs/javas.sln.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if "%script_dir%"=="" (
+    for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
+    call !script_dir!\..\batcolors\echos_macros.bat export
+)
+
+set "p=%~1"
+if not "%p:jdk-8u=%" == "%p%" ( echo jdk8 )
+if not "%p:hotspot_11.=%" == "%p%" ( echo jdk11 )
+if not "%p:hotspot_17.=%" == "%p%" ( echo jdk17 )
