85205fc8e636938d043dea2d348f828a72064753
VonC <vonc@laposte.net>
2024-07-16 09:15:35 +0200

    feat(installs): nodes.sln.bat proposes sln nodexx based on node installation folder

---
+++ installs/nodes.sln.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if "%script_dir%"=="" (
+    for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
+    call !script_dir!\..\batcolors\echos_macros.bat export
+)
+
+set "p=%~1"
+if not "%p:node-v10.=%" == "%p%" ( echo node10 )
+if not "%p:node-v14.=%" == "%p%" ( echo node14 )
