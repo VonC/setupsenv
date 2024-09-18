176d6cef8aff3060230fd33ec6e5231abf7d7840
VonC <vonc@laposte.net>
2024-09-18 10:21:14 +0200

    fix(bat): setup.bat check if cdi is appended to a final empty line

--- setup.bat
+++ setup.bat
@@ -224,6 +224,11 @@ echo HOME='%HOME%'
 echo script_dir='%script_dir%'
 rem set "script_dir=%cd%"
 findstr /V "cdi=" "%HOME%\bin\senv.local.doskey" > "%script_dir%\tmp"
+:: Check if the last line of tmp is empty
+for /f "tokens=* delims=" %%a in ('type "%script_dir%\tmp"') do set "lastLine=%%a"
+:: If the last line is not empty, append an empty line
+if not "%lastLine%"=="" echo.>> "%script_dir%\tmp"
+:: Now append the new line
 echo cdi=cd /d "%script_dir%">> "%script_dir%\tmp"
 for /f "delims=" %%x in (%script_dir%\custom\profile) do set profile=%%x
 set "setupsdirbat=setupsdir_%profile%.bat"
