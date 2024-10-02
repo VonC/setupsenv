fc51dfddff18a37408c6a5cbae00c5638490294c
VonC <vonc@laposte.net>
2024-10-02 08:26:28 +0200

    refactor(gsenv): do not use vscodei var, only vscodes current

--- bin/gsenv.bat
+++ bin/gsenv.bat
@@ -6,7 +6,7 @@ call %script_dir_bin%\senv.bat
 tasklist /FI "IMAGENAME eq Code.exe" /FO CSV|grep Code.exe >NUL
 IF ERRORLEVEL 1 (
 	call "%PRGS%\senv\installs\vscodes.pre.bat" "check"
-	call "%vscodei%bin\code.cmd"
+	call "%PRGS%\vscodes\current\bin\code.cmd"
 	echo VSCode launched
 ) else (
 		echo already launched
