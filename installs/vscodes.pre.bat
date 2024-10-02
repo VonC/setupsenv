f1750b277867b10c042c91f3ab4bd62ab40a4895
VonC <vonc@laposte.net>
2024-10-02 09:47:58 +0200

    refactor(installs): vscodes.pre.bat calls getInstallPath.bat

--- installs/vscodes.pre.bat
+++ installs/vscodes.pre.bat
@@ -1,33 +1,24 @@
 @echo off
-echo pre: vscode
-set "vscodei="
 setlocal enabledelayedexpansion
-for /f "tokens=3*" %%a in ('reg query HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall /v "InstallLocation" /s ^| grep -i code') do (
-    set "vscodei=%%a"
-	rem echo vscodei 0 '%vscodei%' '!vscodei!'
-	if not exist "!vscodei!" ( set "vscodei=%%a %%b")
-	rem echo vscodei 1 '%vscodei%' '!vscodei!'
-	if not exist "!vscodei!" ( set "vscodei=%%a %%b %%c")
-	rem echo vscodei 2 '%vscodei%' '!vscodei!'
-	if not exist "!vscodei!" ( set "vscodei=%%a %%b %%c %%d")
-	rem echo vscodei 3 '%vscodei%' '!vscodei!'
-	if not exist "!vscodei!" ( set "vscodei=%%a %%b %%c %%d %%e")
-	rem echo vscodei 4 '%vscodei%' '!vscodei!'
-	if not exist "!vscodei!" ( set "vscodei=%%a %%b %%c %%d %%e %%f")
-	rem echo vscodei 5 '%vscodei%' '!vscodei!'
+if "%script_dir%"=="" (
+    for %%i in ("%~dp0..") do SET "script_dir=%%~fi"
+		set "empty_pre_ok=true"
 )
-rem echo vscodei final='%vscodei%' '!vscodei!'
-endlocal & set vscodei=%vscodei%
+call %script_dir%\batcolors\echos_macros.bat export
+%_info% "pre: vscode"
 
-for /f  %%a in ('alias vscode') do (
-	set vv=%%a
-)
-rem echo vv='%vv%' '%vscodei%'
-
-if exist "%vscodei%" (
-    set pre_ok=true
+set "pre_ok="
+call %script_dir%\bin\getInstallPath.bat VSCode code
+%_info% "pre: instPath='%instPath%'"
+if exist "%instPath%\bin\code.cmd" (
+  set "pre_ok=true"
 	if "%1"=="" (
-		%_ok% "VSCode already installed in '%vscodei%"
+		%_ok% "VSCode already installed in '%instPath%"
 	)
-	exit /b 0
 )
+endlocal & set "pre_ok=%pre_ok%" & set "empty_pre_ok=%empty_pre_ok%"
+set "vscodei="
+if "%empty_pre_ok%"=="true" (
+	set "pre_ok="
+)
+set "empty_pre_ok="
