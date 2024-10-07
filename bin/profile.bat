0da1f23c1ed362063b72c169e56b90c518bf18c4
VonC <vonc@laposte.net>
2024-10-07 21:11:08 +0200

    fix(bin): profile.bat should not truncate first char of cdis path

--- bin/profile.bat
+++ bin/profile.bat
@@ -3,5 +3,5 @@ setlocal ENABLEDELAYEDEXPANSION
 FOR /F %%i IN ('cat %HOME%\bin\profile') DO set profileName=%%i
 echo profile name='%profileName%'
 FOR /F "tokens=3,* delims= " %%i IN ('alias cdis') DO set profileFolder=%%i
-set "profileFolder=%profileFolder:~1,-1%"
+set "profileFolder=%profileFolder:~0,-1%"
 echo from profile folder '%profileFolder%'
