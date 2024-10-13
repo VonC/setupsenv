51246ed2e9c58d78fc5c3dc33b751ec4285d4feb
VonC <vonc@laposte.net>
2024-10-13 16:29:06 +0200

    chore(installs): gits.config.utils.bat echos shows script name

--- installs/gits.config.utils.bat
+++ installs/gits.config.utils.bat
@@ -8,7 +8,7 @@ call "%senv_dir%\batcolors\echos_macros.bat"
 
 where cat > NUL 2>NUL
 if errorlevel 1 (
-  %_info% "Skip gits.config.utils: cat non available in path"
+  %_info% "[%~nx0] Skip gits.config.utils: cat non available in path"
   goto:eof
 )
 
@@ -30,31 +30,31 @@ goto:eof
 cat %HOME%\.gitconfig >NUL
 grep "st = status" %HOME%\.gitconfig >NUL
 if errorlevel 1 (
-  %_fatal% "Unable to read %HOME%\.gitconfig: content corrupted (%*)" 666
+  %_fatal% "[%~nx0] Unable to read %HOME%\.gitconfig: content corrupted (%*)" 666
 )
 copy /Y %HOME%\.gitconfig %HOME%\.gitconfig.ori >NUL
 if errorlevel 1 (
-  %_fatal% "Unable to copy %HOME%\.gitconfig: content corrupted (%*)" 667
+  %_fatal% "[%~nx0] Unable to copy %HOME%\.gitconfig: content corrupted (%*)" 667
 )
-%_ok% "%HOME%\.gitconfig copied to %HOME%\.gitconfig.ori (%*)"
+%_ok% "[%~nx0] %HOME%\.gitconfig copied to %HOME%\.gitconfig.ori (%*)"
 goto:eof
 
 :restore_gitconfig
 if not exist "%HOME%\.gitconfig.ori" (
-  %_info% "skip  gits.config.utils restore_gitconfig: no .gitconfig.ori"
+  %_info% "[%~nx0] skip  gits.config.utils restore_gitconfig: no .gitconfig.ori"
   goto:eof
 )
 grep "st = status" %HOME%\.gitconfig.ori >NUL
 if errorlevel 1 (
-  %_fatal% "Unable to read %HOME%\.gitconfig.ori: content corrupted (%*)" 668
+  %_fatal% "[%~nx0] Unable to read %HOME%\.gitconfig.ori: content corrupted (%*)" 668
 )
 copy /Y %HOME%\.gitconfig.ori %HOME%\.gitconfig >NUL
 if errorlevel 1 (
-  %_fatal% "Unable to copy %HOME%\.gitconfig.ori: content corrupted (%*)" 669
+  %_fatal% "[%~nx0] Unable to copy %HOME%\.gitconfig.ori: content corrupted (%*)" 669
 )
 grep "st = status" %HOME%\.gitconfig >NUL
 if errorlevel 1 (
-  %_fatal% "Unable to confirm read %HOME%\.gitconfig: content corrupted (%*)" 670
+  %_fatal% "[%~nx0] Unable to confirm read %HOME%\.gitconfig: content corrupted (%*)" 670
 )
-%_ok% "%HOME%\.gitconfig.ori copied to %HOME%\.gitconfig (%*)"
+%_ok% "[%~nx0] %HOME%\.gitconfig.ori copied to %HOME%\.gitconfig (%*)"
 goto:eof
