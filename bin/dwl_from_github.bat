9875bfb2d674f4b6050cf29f5e0c64a5a1702277
VonC <vonc@laposte.net>
2024-10-14 22:50:37 +0200

    wip(bin): dwl_from_github.bat

--- bin/dwl_from_github.bat
+++ bin/dwl_from_github.bat
@@ -4,25 +4,61 @@ setlocal enabledelayedexpansion
 for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
 for %%i in ("%script_dir%\..") do ( set "senv_dir=%%~fi" )
 call %senv_dir%\batcolors\echos_macros.bat
+for %%i in ("%senv_dir%\..\setup") do ( set "setup_dir=%%~fi" )
 
-set "extension=_Windows_x86_64"
+set "repo=%~1"
+set "prgname=%~2"
 
+if "%repo%"=="" (
+    %_fatal% "[%~nx0] repo must be provided (ex: charmbracelet/gum)" 1
+)
 rem @echo on
-set "arg=%~1"
-if "%arg%"=="" ( goto:dwl_from_github)
-if not "%arg::=%"=="%arg%" ( goto%arg% )
-echo nope
+echo.%repo%| findstr /R /C:"^[a-Z0-9_]*/[a-Z0-9_]*$" >nul
+rem echo %ERRORLEVEL%
+if errorlevel 1 (
+    %_fatal% "[%~nx0] repo must be in the format org/repo (ex: charmbracelet/gum)" 1
+)
+
+if "%prgname%"=="" (
+    %_fatal% "[%~nx0] prgname must be provided (ex: gum)" 1
+)
+echo.%prgname%| findstr /R /C:"^[a-Z0-9_]*$" >nul
+rem echo %ERRORLEVEL%
+if errorlevel 1 (
+    %_fatal% "[%~nx0] prgname must be composed of letter, digits or _ (ex: gum)" 1
+)
+
+set "prgsfolder=%prgname%s"
+
+mkdir "%PRGS%\%prgsfolder%" 2> nul
+
+
+set "version=0.14.5"
+goto:dr
+
+
+%_info% "[%~nx0] Check latest '%repo%' version for '%prgname%'"
+rem @echo on
+set "cmd=curl -IkLs -o NUL -w %%{url_effective} https://github.com/%repo%/releases/latest"
+echo.%cmd%
+
+for /f "tokens=* delims=" %%a in ('%cmd%') do ( set gu=%%a)
+echo.Latest version URL='%gu%'
+
+for /f "tokens=1,2,3,4,5,6,7,8 delims=/" %%a in ("%gu%") do set version=%%g
+set "version=%version:v=%"
+
+:dr
+rem set "version=2.35.1.windows.2"
+%_info% "[%~nx0] Latest version='%version%'"
+
+call "%script_dir%\dwl%prgname%.bat" :get_filename %version%
+echo ----
 goto:eof
 
-:dwl_from_github
-set "repo=charmbracelet/gum"
-set "prgname=gum"
-call "%script_dir%\dwl_from_github.bat" "%repo%" "%prgname%"
-goto:eof
-
-:get_filename
-set "version=%~2"
-echo %prgname%_%version%%extension%.zip
+rem Call the script and capture its output
+rem for /f "delims=" %%i in ('call "%script_dir%\dwl%prgname%.bat" :get_filename %version%') do set "file=%%i"
+%_info% "[%~nx0] file='%file%'"
 goto:eof
 
 rem https://github.com/charmbracelet/gum/releases/download/v0.14.1/gum_0.14.1_Windows_x86_64.zip
