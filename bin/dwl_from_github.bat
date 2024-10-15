3ec23fb3083f454557d6ce7e4a9f286bfae58b47
VonC <vonc@laposte.net>
2024-10-15 10:58:01 +0200

    fix(bin): dwl_from_github.bat cleanup, no more installation

--- bin/dwl_from_github.bat
+++ bin/dwl_from_github.bat
@@ -58,7 +58,7 @@ for /f "delims=" %%i in ('call "%script_dir%\dwl%prgname%.bat" :get_filename %ve
 
 if exist "%setup_dir%\%file%" (
     %_ok% "[%~nx0] '%file%' Already downloaded in setup_dir '%setup_dir%'"
-    goto:install
+    goto:eof
 )
 
 set "url=https://github.com/%repo%/releases/download/v%version%/%file%"
@@ -69,48 +69,3 @@ if not "%ERRORLEVEL%" == "0" (
     %_fatal% "[%~nx0] Unable to download '%setup_dir%\%file%' from latest, URL '%url%'" 1
 )
 %_ok% "[%~nx0] '%file%' downloaded to '%setup_dir%'"
-
-:install
-if not defined SENV_DWL_INSTALL (
-    %_ok% "No installation needed (SENV_DWL_INSTALL not defined)"
-    goto:eof
-)
-goto:eof
-
-set "filename=%file:~0,-4%"
-if not exist "%PRGS%\%prgsfolder%\%filename%" (
-    %_task% "[%~nx0] Unzip '%setup_dir%\%file%' to '%PRGS%\%prgsfolder%'"
-    unzip "%setup_dir%\%file%" -d "%PRGS%\%prgsfolder%\%filename%"
-    if ERRORLEVEL 1 (
-        %_fatal% "[%~nx0] Unable to unzip '%setup_dir%\%file%' to '%PRGS%\%prgsfolder%'" 1
-    )
-    %_ok% "[%~nx0] '%setup_dir%\%file%' unzipped to '%PRGS%\%prgsfolder%'"
-) else (
-    %_ok% "[%~nx0] %PRGS%\%prgsfolder%\%filename%" already unzipped
-)
-
-for /f "tokens=5,* delims= " %%a in ('dir "%PRGS%\%prgsfolder%" ^| grep current') do ( set "curr=%%a" )
-if "%curr%"=="" (
-    %_task% "[%~nx0] No current junction folder detected: must create '%PRGS%\%prgsfolder%\current'"
-)
-rem echo filename='%filename%'
-rem echo curr='%curr%'
-echo.%curr% | findstr /C:"%filename%" >nul
-if %errorlevel% equ 0 (
-    %_ok% "[%~nx0] %filename% is already referenced by junction current"
-    goto:final
-)
-%_warning% "[%~nx0] current (%curr%) does not reference '%filename%' yet"
-%_task% "[%~nx0] '%PRGS%\%prgsfolder%\current' must reference '%filename%'"
-if exist "%PRGS%\%prgsfolder%\current" (
-    rmdir "%PRGS%\%prgsfolder%\current"
-)
-:createcurr
-mklink /J "%PRGS%\%prgsfolder%\current" "%PRGS%\%prgsfolder%\%filename%\%filename%"
-if ERRORLEVEL 1 (
-    %_fatal% "[%~nx0] Unable to create current referencing '%PRGS%\%prgsfolder%\%filename%\%filename%'" 2
-)
-%_ok% "[%~nx0] current now references '%PRGS%\%prgsfolder%\%filename%\%filename%'"
-:final
-%_ok% "[%~nx0] %prgname% available at '%PRGS%\gums\current:"
-%PRGS%\%prgsfolder%\current\%prgname%.exe --version
