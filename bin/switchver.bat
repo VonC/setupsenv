ca57a6717be8f216b27bd16dfb128bca3bab39b9
VonC <vonc@laposte.net>
2024-10-16 16:45:58 +0200

    fix(bin): switchver.bat manages standalone, prints nx0 and uses batcolors

--- bin/switchver.bat
+++ bin/switchver.bat
@@ -1,53 +1,42 @@
 @echo off
 
-set "standalone_switchver="
-if "%script_dir%"=="" (
-    set "standalone_switchver=true"
-)
+if "%script_dir%"=="" ( echo.>>"%~dp0standalone_%~nx0.flag")
 setlocal enabledelayedexpansion
 
 for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
-if exist %script_dir%\..\batcolors (
-  call %script_dir%\..\batcolors\echos_macros.bat
-) else if exist %script_dir%\batcolors (
-  call %script_dir%\batcolors\echos_macros.bat
-) else if exist %script_dir%\echos_macros.bat (
-  call %script_dir%\echos_macros.bat
-) else (
-  echo "[switchver] batcolor not found in script_dir '%script_dir%'" >&2
-  endlocal
-  exit /b 1
-)
+for %%i in ("%script_dir%\..") do ( set "senv_dir=%%~fi" )
+call %senv_dir%\batcolors\echos_macros.bat
+set "echos_standalone=%~dp0standalone_%~nx0.flag"
 
 set "usage="
 set "prgs_name=%~1"
 if "%prgs_name%"=="" (
-    %_error% "switchver first param prgs_name (ex: 'pythons' or 'javas') is MISSING"
+    %_error% "[%~nx0] switchver first param prgs_name (ex: 'pythons' or 'javas') is MISSING"
     set "usage=1"
 )
 if not "%prgs_name:~-1%"=="s" (
-    %_error% "switchver first param prgs_name must ends with an s (ex: 'pythons' or 'javas')"
+    %_error% "[%~nx0] switchver first param prgs_name must ends with an s (ex: 'pythons' or 'javas')"
     set "usage=1"
 )
 set "prg_prefix=%~2"
 if "%prg_prefix%"=="" (
-    %_error% "switchver second param prg_prefix (ex: 'py' or 'jdk') is MISSING"
+    %_error% "[%~nx0] switchver second param prg_prefix (ex: 'py' or 'jdk') is MISSING"
     set "usage=1"
 )
 set "prg_pattern=%~3"
 if "%prg_pattern%"=="" (
-    %_error% "switchver third param prg_pattern (ex: 'python[2-9].[0-9]*$' '^jdk[0-9][0-9]*$') is MISSING"
+    %_error% "[%~nx0] switchver third param prg_pattern (ex: 'python[2-9].[0-9]*$' '^jdk[0-9][0-9]*$') is MISSING"
     set "usage=1"
 )
 set "prg_exe=%~4"
 if "%prg_exe%"=="" (
-    %_error% "switchver fourth param prg_exe (ex: 'java' 'python', do not add the '.exe') is MISSING"
+    %_error% "[%~nx0] switchver fourth param prg_exe (ex: 'java' 'python', do not add the '.exe') is MISSING"
     set "usage=1"
 )
 set "prg_version=%~5"
 if defined usage (
     :: Example: switchver pythons python "python[2-9]\.[0-9]*\.[0-9]*$" python
-    %_fatal% "Usage: switchver prgs_name prg_prefix prg_pattern prg_exe [prg_version]" 2
+    %_fatal% "[%~nx0] Usage: switchver prgs_name prg_prefix prg_pattern prg_exe [prg_version]" 2
 )
 set "usage="
 set "PRGS_ROOT=%PRGS%\%prgs_name%"
@@ -62,10 +51,10 @@ for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
 set "prg_name=%first_letter%%prgs_name:~1%"
 :: Remove the last letter 
 set "prg_name=%prg_name:~0,-1%"
-rem %_fatal% "prgs_name='%prgs_name%' vs. prg_name=%prg_name%'" 1
+rem %_fatal% "[%~nx0] prgs_name='%prgs_name%' vs. prg_name=%prg_name%'" 1
 
-pushd %PRGS_ROOT% || %_fatal% "unable to cd to PRGS_ROOT '%PRGS_ROOT%'" 1
-%_info% "Switch Ver from PRGS_ROOT '%PRGS_ROOT%'"
+pushd %PRGS_ROOT% || %_fatal% "[%~nx0] unable to cd to PRGS_ROOT '%PRGS_ROOT%'" 1
+%_info% "[%~nx0] Switch Ver from PRGS_ROOT '%PRGS_ROOT%'"
 rem @echo on
 rem Initialize counter
 set count=0
@@ -76,7 +65,7 @@ set PRG_VERSIONS=
 :: https://www.dostips.com/forum/viewtopic.php?t=8940
 for /d %%f in (%prg_prefix%*) do (
     set "dirname=%%~nxf"
-    rem %_info% "dirname='!dirname!'"
+    rem %_info% "[%~nx0] dirname='!dirname!'"
     echo !dirname!| findstr /r "%prg_pattern%" >nul
     if not errorlevel 1 (
         set "PRG_VERSIONS=!PRG_VERSIONS! %%f"
@@ -91,12 +80,12 @@ echo "PRG_VERSIONS='%PRG_VERSIONS%', SELECTED_VERSION='%SELECTED_VERSION%'"
 
 if not "%SELECTED_VERSION%" == "" ( goto:selected )
 if not "%prg_version%" == "" (
-    %_warning% "Your %prg_name% version argument '%prg_version%' was NOT found in PRGS_ROOT '%PRGS_ROOT%'"
+    %_warning% "[%~nx0] Your %prg_name% version argument '%prg_version%' was NOT found in PRGS_ROOT '%PRGS_ROOT%'"
 )
 
 :: if count == 1, set SELECTED_VERSION to PRG_VERSIONS, and trim any space
 if %count% equ 1 (
-    %_info% "Only one %prg_name% version found: '%PRG_VERSIONS: =%'"
+    %_info% "[%~nx0] Only one %prg_name% version found: '%PRG_VERSIONS: =%'"
     for %%v in (%PRG_VERSIONS%) do (
         set "SELECTED_VERSION=%%~v"
     )
@@ -104,12 +93,12 @@ if %count% equ 1 (
 )
 
 if %count% equ 0 (
-    %_fatal% "No %prg_version% version found in '%PRGS_ROOT%'" 3
+    %_fatal% "[%~nx0] No %prg_version% version found in '%PRGS_ROOT%'" 3
 )
 
-rem %_info% "PRG_VERSIONS='%PRG_VERSIONS%', SELECTED_VERSION='%SELECTED_VERSION%'"
+rem %_info% "[%~nx0] PRG_VERSIONS='%PRG_VERSIONS%', SELECTED_VERSION='%SELECTED_VERSION%'"
 if "%SELECTED_VERSION%" == "" (
-    %_task% "Select %prg_name% version amongst '%count%' available"
+    %_task% "[%~nx0] Select %prg_name% version amongst '%count%' available"
     :: Use gum for selection
     set "gum=%PRGS%\gums\current\gum.exe"
     for /f "tokens=*" %%a in ('!gum! choose %PRG_VERSIONS%') do set SELECTED_VERSION=%%a
@@ -118,9 +107,9 @@ if "%SELECTED_VERSION%" == "" (
 :selected
 
 if "%SELECTED_VERSION%" == "" (
-    %_fatal% "No %prg_name% version selected for PRGS_ROOT '%PRGS_ROOT%'" 3
+    %_fatal% "[%~nx0] No %prg_name% version selected for PRGS_ROOT '%PRGS_ROOT%'" 3
 )
-%_ok% "%prg_name% version chosen: '%SELECTED_VERSION%'"
+%_ok% "[%~nx0] %prg_name% version chosen: '%SELECTED_VERSION%'"
 @echo on
 :clean_path
 set "newPath="
@@ -132,7 +121,7 @@ for /f "tokens=*" %%j in ('where %prg_exe%') do (
 )
 
 if not "%newPath%" == "" (
-    %_ok% "%prg_name% '%SELECTED_VERSION%' already in PATH"
+    %_ok% "[%~nx0] %prg_name% '%SELECTED_VERSION%' already in PATH"
     goto:skip_clean_path
 )
 
@@ -156,10 +145,10 @@ set "current_path="
 :skip_clean_path
 endlocal & set "newPath=%newPath%" & set "SELECTED_VERSION=%SELECTED_VERSION%"
 popd
-if "%standalone_switchver%"=="true" (
+if exist "%~dp0standalone_%~nx0.flag" (
     echo newPath='%newPath%'
     set "newPath="
     echo SELECTED_VERSION='%SELECTED_VERSION%'
     set "SELECTED_VERSION="
+    del "%~dp0standalone_%~nx0.flag"
 )
-set "standalone_switchver="
