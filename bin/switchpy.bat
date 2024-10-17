88da55f97c63205b52aef3e2817a2e8593f7516f
VonC <vonc@laposte.net>
2024-10-17 22:02:56 +0200

    fix(bin): switchpy.bat reset PATH to CLEANED_PATH

--- bin/switchpy.bat
+++ bin/switchpy.bat
@@ -12,20 +12,20 @@ set PYTHON_ROOT=%PRGS%\pythons
 pushd %PYTHON_ROOT%
 
 call "%script_dir%\switchver.bat" pythons python "python[2-9]\.[0-9]*\.[0-9]*$" python
-echo done
 %_ok% "[%~nx0] Python version chosen: '%SELECTED_VERSION%'"
 set "PYTHON_HOME=%PRGS%\pythons\%SELECTED_VERSION%"
 set "PYTHON_VERSION=3%SELECTED_VERSION:*3=%"
 
 endlocal & set "PYTHON_HOME=%PYTHON_HOME%" & set "PYTHON_VERSION=%PYTHON_VERSION%" & set "PATH=%newPath%"
 
-echo PYTHON_HOME='%PYTHON_HOME%'
-echo CLEANED PATH='%PATH%'
+rem echo PYTHON_HOME='%PYTHON_HOME%'
+rem echo CLEANED PATH='%PATH%'
 set PYTHON_ROOT=%PRGS%\pythons
+set PYTHON_VENVS=%PYTHON_ROOT%\venvs
 set "_OLD_VIRTUAL_PATH=%PATH%"
 
-mkdir "%PYTHON_ROOT%\venvs" 2>nul
-pushd %PYTHON_ROOT%\venvs
+mkdir "%PYTHON_VENVS%" 2>nul
+pushd %PYTHON_VENVS%
 set PYTHON_MAIN_VERSION=
 :: Split PYTHON_VERSION by '.' and keep the first two segments
 for /f "tokens=1,2 delims=." %%a in ("%PYTHON_VERSION%") do (
@@ -33,13 +33,13 @@ for /f "tokens=1,2 delims=." %%a in ("%PYTHON_VERSION%") do (
 )
 echo PYTHON_MAIN_VERSION='%PYTHON_MAIN_VERSION%'
 if not "%VIRTUAL_ENV%" == "" (
-    if "%VIRTUAL_ENV%" == "%PRGS%\pythons\venvs\python_%PYTHON_VERSION%" (
+    if "%VIRTUAL_ENV%" == "%PYTHON_VENVS%\python_%PYTHON_VERSION%" (
         echo OK: Py env for 'Python %PYTHON_VERSION%' already activated
         popd
         exit /b 0
     ) else (
         echo deactivate: different venv activated: '%VIRTUAL_ENV%'
-        call "%PRGS%\pythons\venvs\python_%PYTHON_VERSION%\Scripts\deactivate.bat"
+        call "%PYTHON_VENVS%\python_%PYTHON_VERSION%\Scripts\deactivate.bat"
         if errorlevel 1 (
             echo ERROR: Unable to deactivate Py env for 'Python %PYTHON_VERSION%'
             popd
