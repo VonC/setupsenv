cc4a6d37a70ed4305d3da28cb1d4a79c93379078
VonC <vonc@laposte.net>
2024-10-22 10:51:42 +0200

    fix(bin): profile.bat manages remote path with spaces

--- bin/profile.bat
+++ bin/profile.bat
@@ -7,23 +7,40 @@ call %home_dir%\batcolors\echos_macros.bat
 
 FOR /F %%i IN ('cat %HOME%\bin\profile') DO set profileName=%%i
 %_info% "[%~nx0] profile name='%profileName%'"
-FOR /F "tokens=3,* delims= " %%i IN ('alias cdis') DO set profileFolder=%%i
-set "profileFolder=%profileFolder:~0,-1%"
-%_info% "[%~nx0] from profile folder '%profileFolder%'"
+:: Use FOR /F to capture all tokens starting from the third one, managing spaces in the profile folder name
+FOR /F "tokens=3* delims= " %%i IN ('alias cdis') DO (
+    set "profileFolder=%%i"
+    set "rest=%%j"
+    if defined rest (
+        for %%k in (!rest!) do (
+            set "profileFolder=!profileFolder! %%k"
+        )
+    )
+)
+set "profileFolder=%profileFolder:"=%"
+%_info% "[%~nx0] from profile setups folder '%profileFolder%'"
 
 
-rem for /f "tokens=* delims=" %%i in ('git -C "%PRGS%\senv\custom" describe --long --all HEAD') do SET "vcsenv=%%i"
-rem for /f "tokens=* delims=" %%i in ('git -C "%PRGS%\senv" describe --long --all HEAD') do SET "vcsenv=!vcsenv! - %%i"
+for /f "tokens=* delims=" %%i in ('git -C "%PRGS%\senv\custom" describe --long --all HEAD') do SET "vcsenv=%%i"
+for /f "tokens=* delims=" %%i in ('git -C "%PRGS%\senv" describe --long --all HEAD') do SET "vcsenv=!vcsenv! - %%i"
 for /f "tokens=* delims=" %%i in ('type "%PRGS%\senv\custom\version"') do SET "vc=%%i"
 %_info% "[%~nx0] version '%vc%', locale, at '%PRGS%\senv\custom'"
 
 for %%i in ("%profileFolder%\..") do ( set "remote_senv=%%~fi" )
-for /f "tokens=* delims=" %%i in ('type "%remote_senv%\version"') do SET "remote_vc=%%i"
-%_info% "[%~nx0] version '%vc%', remote, at '%remote_senv%'"
-
+if exist "%remote_senv%\version" (
+    for /f "tokens=* delims=" %%i in ('type "%remote_senv%\version"') do SET "remote_vc=%%i"
+    %_info% "[%~nx0] version '!remote_vc!', remote, at '%remote_senv%'"
+) else (
+    %_warning% "No version found at remote senv '%remote_senv%'"
+)
 if "%vc%"=="%remote_vc%" (
     %_ok% "[%~nx0] senv is up to date"
 ) else (
     %_warning% "[%~nx0] senv is not up to date"
-    goto:update_senv
+)
+
+if not "%vcsenv%"=="%remote_vc%" (
+    %_warning% "[%~nx0] local senv updated: needs to be published"
+) else (
+    %_ok% "[%~nx0] local senv unchanged"
 )
