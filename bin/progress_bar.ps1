2e38acdeaa7e240cfdc6e9de11793a3762a5f245
VonC <vonc@laposte.net>
2024-10-30 09:11:08 +0100

    feat(bin): progress_bar.ps1 add more color, and can be called just for one value

--- bin/progress_bar.ps1
+++ bin/progress_bar.ps1
@@ -1,13 +1,22 @@
 param (
-    [int]$n = 100
+    [int]$current = -1,
+    [int]$total = 100
 )
-
 $PSModuleAutoloadingPreference = 'None'
-# powershell -ExecutionPolicy Bypass -File progress_bar.ps1 -n 50
+# powershell -ExecutionPolicy Bypass -File progress_bar.ps1 -current 50 -total 100
+#
+# powershell -ExecutionPolicy Bypass -File %PRGS%\senv\bin\progress_bar.ps1 -total 50
+# powershell -ExecutionPolicy Bypass -File %PRGS%\senv\bin\progress_bar.ps1 -total 50 -current 12
+# 
 # Load necessary assembly (suppress output)
 # [void][Reflection.Assembly]::LoadWithPartialName("System.Select-Object")
 Import-Module CimCmdlets
 Import-Module Microsoft.PowerShell.Utility
+
+trap {
+  Write-Host "CTRL-C detected. Exiting gracefully..."
+  exit
+}
 
 # Define the escape character
 $e = [char]27
@@ -24,23 +33,23 @@ function Show-ProgressBar {
 
     # Determine color based on percentage
     if ($percent -le 10) {
-      $color = "$e[38;5;196m"  # Deep Red
+        $color = "$e[38;5;196m"  # Deep Red
     } elseif ($percent -le 20) {
-      $color = "$e[38;5;202m"  # Light Red
+        $color = "$e[38;5;202m"  # Light Red
     } elseif ($percent -le 30) {
-      $color = "$e[38;5;208m"  # Light Orange
+        $color = "$e[38;5;208m"  # Light Orange
     } elseif ($percent -le 40) {
-      $color = "$e[38;5;214m"  # Orange
+        $color = "$e[38;5;214m"  # Orange
     } elseif ($percent -le 50) {
-      $color = "$e[38;5;226m"  # Yellow
+        $color = "$e[38;5;226m"  # Yellow
     } elseif ($percent -le 60) {
-      $color = "$e[38;5;228m"  # Light Yellow
+        $color = "$e[38;5;228m"  # Light Yellow
+    } elseif ($percent -le 70) {
+        $color = "$e[38;5;154m"  # Light Green
     } elseif ($percent -le 80) {
-      $color = "$e[38;5;154m"  # Light Green
-    } elseif ($percent -le 90) {
-      $color = "$e[38;5;118m"  # Green
+        $color = "$e[38;5;118m"  # Green
     } else {
-      $color = "$e[38;5;34m"   # Dark Green
+        $color = "$e[38;5;34m"   # Dark Green
     }
 
     $bar = "$color" + ("#" * $filledLength) + "$e[0m"
@@ -50,7 +59,11 @@ function Show-ProgressBar {
     Write-Host -NoNewline "`r[$bar$padding] $percent% ($current/$total)"
 }
 
-for ($i = 0; $i -le $n; $i++) {
-    Show-ProgressBar -current $i -total $n
-    Start-Sleep -Milliseconds 100
+if ($current -eq -1) {
+    for ($i = 0; $i -le $total; $i++) {
+        Show-ProgressBar -current $i -total $total
+        Start-Sleep -Milliseconds 100
+    }
+} else {
+    Show-ProgressBar -current $current -total $total
 }
