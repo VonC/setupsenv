f24d0a850ad305b35d1e5ae392df8fb18fcf1d3c
VonC <vonc@laposte.net>
2024-10-30 09:17:47 +0100

    fix(bin): resumep.bat no longer call progress_bar, only resumep.sh

--- bin/resumep.bat
+++ bin/resumep.bat
@@ -6,6 +6,7 @@ cd /d "%script_dir%" || echo "unable to cd to '%script_dir%'"&& exit /b 1
 
 set "resumep_loop="
 set "prg=%~1"
+if not defined prg ( goto:call_run )
 if not "%prg:loop=%"=="%prg%" (
   set "resumep_loop=%prg:loop_=%"
   if "!resumep_loop!"=="loop" (
@@ -16,16 +17,17 @@ if not "%prg:loop=%"=="%prg%" (
 set "prg=%~1"
 rem echo prg=%prg%, resumep_loop=%resumep_loop%
 rem goto:eof
+:call_run
 call <NUL :rrun "%prg%"
 endlocal
 goto:eof
 
 :rrun
 :loop
-bash -f "%script_dir%\resumep.sh" "%~1"
-if defined resumep_loop (
-  echo Waiting %resumep_loop% seconds before retrying
-  ping -n %resumep_loop% 8.8.8.8 > nul || exit /b 0
-  goto:loop
+bash -f "%script_dir%\resumep.sh" "%~1" %resumep_loop%
+if errorlevel 1 (
+  echo Errorlevel: %errorlevel%
+  goto:eof
 )
+if defined resumep_loop ( goto:loop )
 goto:eof
