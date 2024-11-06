ee07bf75d72336e1bb6438052ef64a1a2a473346
VonC <vonc@laposte.net>
2024-11-06 10:30:42 +0100

    fix(bin): select_prg.bat select prg line

--- bin/select_prg.bat
+++ bin/select_prg.bat
@@ -35,24 +35,57 @@ call :read_file %file2%
 
 REM Step 1: check if a program name is provided as argument
 
-set "prgname=%~1"
-if "%prgname%"=="" (
-  goto:select_program
+set "prg_name=%~1"
+if "%prg_name%"=="" (
+  call:select_program
 )
+set "prg_line="
+call:parse_prgs_list "%script_dir%\prgs.list"
+if not defined prg_line (
+  if exist "%senv_home%\prgs.list" (
+    call:parse_prgs_list "%script_dir%\prgs.list"
+  )
+)
+if not defined prg_line (
+  %_fatal% "[%~nx0] prg_name '%prg_name%' not found in available program list" 11
+)
+%_ok% "[%~nx0] prg_name '%prg_name%' matches prg_line '%prg_line%'"
+
+for /f "tokens=1-4 delims=~" %%a in ('echo %prg_line%') do (
+  set "prg_names=%%a"
+  set "prg_versions=%%b"
+  set "prg_folders=%%c"
+  set "prg_patterns=%%d"
+)
+if defined prg_versions ( set "prg_versions=%prg_versions:#=%" )
+if defined prg_folders ( set "prg_folders=%prg_folders:#=%" )
+if defined prg_patterns ( set "prg_patterns=%prg_patterns:#=%" )
+%_info% "[%~nx0] prg_names='%prg_names%', prg_versions='%prg_versions%', prg_folders='%prg_folders%', prg_patterns='%prg_patterns%'"
+
+
+goto:eof
 rem findstr /R /C:"^Git/" prgs.list
 rem findstr /R /C:"/git[/,]" prgs.list
-rem TODO for /f "delims=" %%p in ('findstr /R /C:"/git[/,]" %script_dir%\prgs.list') do set "prgname=%%p"
+rem TODO for /f "delims=" %%p in ('findstr /R /C:"/git[/,]" %script_dir%\prgs.list') do set "prg_name=%%p"
 rem if p not nul, goto:select_version
+
+:parse_prgs_list
+for /f "delims=" %%p in ('findstr /I /R /C:"^%prg_name%[/,]" "%~1"') do set "prg_line=%%p"
+if not defined prg_line (
+  for /f "delims=" %%p in ('findstr /I /R /C:"/%prg_name%[/,]" "%~1"') do set "prg_line=%%p"
+)
+goto:eof
 
 :select_program
 REM Select a program using gum
 rem echo type %script_dir%\prg_names.tmp ^| "%PRGS%\gums\current\gum.exe" choose --limit=1
 rem goto:eof
-for /f "delims=" %%p in ('bash -c "'%PRGS%\gums\current\gum.exe' choose --limit 1 $(sed "s/\S+.*?$\r\n/\n/g" prg_names.tmp)"') do set "prgname=%%p"
-if "%prgname%"=="" (
+sed "s/[/,].*$//g" prgs.list > prg_names.tmp
+for /f "delims=" %%p in ('bash -c "'%PRGS%\gums\current\gum.exe' choose --limit 1 $(sed "s/\S+.*?$\r\n/\n/g" prg_names.tmp)"') do set "prg_name=%%p"
+if "%prg_name%"=="" (
   %_fatal% "[%~nx0] No program selected" 1
 )
-%_ok% "[%~nx0] Program selected: %prgname%"
+%_ok% "[%~nx0] Program selected: %prg_name%"
 goto:eof
 
 REM Function to read and parse a file
