98586358ac936c1135c35881462c7ecd3d7eba32
VonC <vonc@laposte.net>
2024-11-20 09:28:10 +0100

    fix(bin): gsenv.bat launches VSCode after custom and local gsenv

--- bin/gsenv.bat
+++ bin/gsenv.bat
@@ -2,6 +2,9 @@
 for %%i in ("%~dp0.") do SET "script_dir_bin=%%~fi"
 call %script_dir_bin%\senv.bat
 
+call %HOME%\bin\gsenv.custom.bat
+call %HOME%\bin\gsenv.local.bat
+
 <nul set /p =VSCode...
 tasklist /FI "IMAGENAME eq Code.exe" /FO CSV|grep Code.exe >NUL
 IF ERRORLEVEL 1 (
@@ -12,8 +15,5 @@ IF ERRORLEVEL 1 (
 ) else (
 		echo already launched
 )
-
-call %HOME%\bin\gsenv.custom.bat
-call %HOME%\bin\gsenv.local.bat
 
 echo All gsenv done
