b25f0de3817495e794a15702a24ced96bb9db6cc
VonC <vonc@laposte.net>
2024-11-30 12:12:23 +0100

    chore(bat): setup.bat comment typo

--- setup.bat
+++ setup.bat
@@ -82,7 +82,7 @@ echo @echo off%NL%call %HOME%\bin\senv.bat> "%USERPROFILE%\senv.bat"
 echo @echo off%NL%call %HOME%\bin\gsenv.bat> "%USERPROFILE%\gsenv.bat"
 echo @echo off%NL%call "%%USERPROFILE%%\senv.bat"> "%HOME%\senv.bat"
 echo @echo off%NL%call "%%USERPROFILE%%\gsenv.bat"> "%HOME%\gsenv.bat"
-REM usefull when HOMEDRIVE is U: or other than C:
+REM useful when HOMEDRIVE is U: or other than C:
 if not "%HOMEDRIVE%"=="C:" (
     echo @echo off%NL%call "%%USERPROFILE%%\senv.bat"> "%HOMEDRIVE%\senv.bat"
     echo @echo off%NL%call "%%USERPROFILE%%\gsenv.bat"> "%HOMEDRIVE%\gsenv.bat"
