90f6ae550a0a30a6c8ad9800f61399bbe6cec68a
VonC <vonc@laposte.net>
2024-12-18 14:05:21 +0100

    fix(bin): yed.bat extracts yED version properly

--- bin/yed.bat
+++ bin/yed.bat
@@ -3,7 +3,9 @@ call "%HOME%\bin\switchjdk.bat" 22
 if errorlevel 1 (
     call:unset && call "%HOME%\batcolors\echos.bat" :fatal "Unable to switch to JDK 22" 11
 )
-for "tokens=1 delims=-" %%a in ('ls -l "%PRGS%\yeds\current"') do ( set "yed_version=%%a" )
+for /f "tokens=* delims=-" %%a in ('ls -l "%PRGS%\yeds\current"') do ( set "yed_version=%%a" )
+set "yed_version=%yed_version:*/yeds/yED-=%"
+for /f "tokens=1 delims=_" %%a in ('echo %yed_version%') do ( set "yed_version=%%a" )
 start "yed" /B cmd /C %PRGS%\yeds\current\yed.exe %*
 if errorlevel 1 (
     call:unset && call "%HOME%\batcolors\echos.bat" :fatal "Unable to launch yEd version '%yed_version%'" 12
