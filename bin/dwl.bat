4ee44e751dd18d86eee9649135f74ad8c1c7900c
VonC <vonc@laposte.net>
2024-12-19 08:23:43 +0100

    fix(bin): dwl.bat add URL in logs

--- bin/dwl.bat
+++ bin/dwl.bat
@@ -151,11 +151,14 @@ if exist "%setup_dir%\%target_local_file%" (
 )
 
 %_task% "[%~nx0] Download latest to '%setup_dir%\%target_local_file%' from URL '!url!'"
+rem bash -c "a="2,3"; echo _${a/,/%}_"
 rem @echo on
-curl -fkL %url% -o "%setup_dir%\%target_local_file%"
+rem for /f "delims=" %%a in ('cygpath -u "%setup_dir%\%target_local_file%"') do set "target_full_unix_path=%%a"
+rem bash -c "url="%url%"; url="${url/,/^%%}"; echo "url='${url}'"; dst="%target_full_unix_path%"; curl -fkL "${url}" -o "${dst}""
+curl -fkL "%url%" -o "%setup_dir%\%target_local_file%"
 if not "%ERRORLEVEL%" == "0" (
   if not defined next_url (
-    %_fatal% "[%~nx0] Unable to download '%setup_dir%\%target_local_file%' from latest, URL '%url%'" 1
+    %_fatal% "[%~nx0] Unable to download '%setup_dir%\%target_local_file%' from latest, URL '%url%'" 191
   ) else (
     %_error% "[%~nx0] Unable to download '%setup_dir%\%target_local_file%' from latest, URL '%url%'
     exit /b 1
@@ -317,7 +320,7 @@ rem https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.25
 set "url=https://github.com/adoptium/temurin%jdk_version%-binaries/releases/download/%dVersion%/%file%"
 %_ok% "[%~nx0] Built latest jdk version URL='%url%'"
 
-%_task% "[%~nx0] Must get latest URL from adoptium.net for jdk version '%jdk_version%'"
+%_task% "[%~nx0] Must get latest URL from adoptium.net for jdk version '%jdk_version%', url='%url%'"
 rem @echo on
 for /f "delims=" %%a in ('curl -sLk "https://api.adoptium.net/v3/assets/latest/%jdk_version%/hotspot?architecture=x64&image_type=jdk&os=windows&vendor=eclipse" ^| findstr /R /C:"[^_]link.*zip"') do ( set "gu=%%a" )
 set "gu=%gu:*: =%"
