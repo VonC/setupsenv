2b4e10a3e8edc7a4b422e62441699c51e3f14bd9
VonC <vonc@laposte.net>
2025-01-22 10:03:20 +0100

    feat(bin,alias): gst show a tag annotation, alias gstr replaces date with tag

---
+++ bin/gst.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
+for %%i in ("%script_dir%\..") do ( set "senv_dir=%%~fi" )
+call %senv_dir%\batcolors\echos_macros.bat
+for %%i in ("%PRGS%\setup") do (
+    set "setup_dir=%%~fi"
+)
+
+set "tag_name=%1"
+if not defined tag_name (
+  %_fatal% "A tag name is needed" 1
+)
+
+if defined REPLACE_DATE ( goto:replace_date )
+bash -c 'res=$(git show -s --format=%%N "${tag_name}" ^| tail -n +4); echo -n "${res}"'
+goto:eof
+
+:replace_date
+bash -c 'res=$(git show -s --format=%%N "${tag_name}" ^| tail -n +4 ^| sed "1s/^.*\? --/${tag_name} --/"); echo -n "${res}"'
