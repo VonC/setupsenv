3de9e2513118949ae96fd4dd2a7abd63868d3912
VonC <vonc@laposte.net>
2025-03-14 02:22:03 +0100

    feat(senv): npp alias/EDITOR with settings folder
    Why: Set an alias and EDITOR variable, using `-settingsDir="%PRGS%\npps\settings"`
    if it exists, to facilitate the migration to a new npp instance when installing
    a new npp version.
    
    What:
    - Added a new setting to tell notepad++ to read and write files in a dedicated
      portable directory '%PRGS%\npps\settings'
    - Updated the `npp` doskey command to support arguments
    - Updated the `EDITOR` environment variable to launch notepad++ with the new
      settings (if the folder exists)

--- bin/senv.bat
+++ bin/senv.bat
@@ -55,6 +55,12 @@ set sz=%pz%\res\7z\7z.exe
 
 set "EDITOR=%PRGS%\vscodes\current\bin\code.cmd"
 set "EDITOR="%PRGS%\npps\current\notepad++.exe" -multiInst -notabbar -nosession -noPlugin"
+doskey npp="%PRGS%\npps\current\notepad++.exe" $*
+
+if exist "%PRGS%\npps\settings" (
+   set "EDITOR="%PRGS%\npps\current\notepad++.exe" -settingsDir="%PRGS%\npps\settings" -multiInst -notabbar -nosession -noPlugin"
+   doskey npp="%PRGS%\npps\current\notepad++.exe" -settingsDir="%PRGS%\npps\settings" $*
+)
 
 if exist "%PRGS%\gos\current" (
    set CGO_ENABLED=1
