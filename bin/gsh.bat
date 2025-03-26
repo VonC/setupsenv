754f245b7be371944d841b20ce36c2e8858403d4
VonC <vonc@laposte.net>
2025-03-26 14:05:17 +0100

    fix(bin): gsh.bat prompt must not depend on mods.exe
    Why
    The "prompt" mode can still try and seek a prompts from the `mods.exe` settings.
    But when said `mods.exe` is /not/ installed, we must generate a prompt locally.
    Previously, the script would fail in these situations.
    The updated script now provides more informative warning messages and creates a
    default prompt if necessary.
    
    Reminder: the prompt mod is made to dump a prompt + a diff to the user clipboard,
    for said user to paste it to any LLM web page of their chosing.
    
    What
    - Added error handling for missing `mods.exe` by checking its availability.
    - Added more informative warning messages if `mods.exe`, `mods.yml`,
      or `config_path` is not found.
    - Implemented a fallback mechanism to create a default prompt from within
      `gsh.bat` if `mods.yml` is not found,
    - Added space around comparison operator in `if "%~1"=="prompt"`.
    - Added `del tmp.txt 2>NUL` before `where mods.exe`, to ensure its creation.
    - Use explicitly `mods.exe --dirs`, not just `mods --dirs`.

--- bin/gsh.bat
+++ bin/gsh.bat
@@ -13,7 +13,7 @@ if %ERRORLEVEL% == 0 (
   %_fatal% "No changes to commit" 11
 )
 
-if "%~1"=="prompt" ( call:dump_prompt && exit /b 0 )
+if "%~1" == "prompt" ( call:dump_prompt && exit /b 0 )
 
 set "npp_settings="
 if exist "%PRGS%\npps\settings" set "npp_settings= -settingsDir="%PRGS%\npps\settings""
@@ -54,16 +54,42 @@ if %ERRORLEVEL% == 1 (
 goto:eof
 
 :dump_prompt
-for /f "tokens=2* delims=:" %%a in ('mods --dirs ^| findstr "Configuration"') do (
+del tmp.txt 2>NUL
+where mods.exe
+if "%ERRORLEVEL%" == "1" (
+    %_warning% "mods.exe not available"
+    goto:create_tmp_txt
+)
+for /f "tokens=2* delims=:" %%a in ('mods.exe --dirs ^| findstr "Configuration"') do (
     set "config_path=%%a:%%b"
 )
 REM Optionally remove any leading space
 set "config_path=%config_path:~1%"
 echo %config_path%
 if not exist "%config_path%\mods.yml" (
-    %_fatal% "Configuration directory not found: '%config_path%\mods.yml'" 15
+    %_warning% "Mods configuration directory not found: '%config_path%\mods.yml'"
+    goto:create_tmp_txt
 )
 awk -ve= "/cm-shell:/ { flag=1 } flag { if ($0 ~ /^[[:space:]]*#/) exit; if ($0 ~ /^[[:space:]]*-[[:space:]]/) { line=$0; sub(/^[[:space:]]*-[[:space:]]/, e, line); if (line ^!= e) print line } }" "%config_path%\mods.yml" > tmp.txt
+:create_tmp_txt
+@echo off
+if not exit tmp.txt (
+  %_task% "Must create prompt from gsh.bat itself"
+  (
+    echo you are a shell, Linux or CMD Windows, Golang, Python and Git expert
+    echo you analyse the Git diff provided and generate a commit message based on the 'conventional commit' convention
+    echo Reminder, conventional commit means, the title must start with 'type-optional scope, description', with 52 characters max
+    echo Types other than `fix:` and `feat:` are `build:`, `chore:`, `ci:`, `docs:`, `style:`, `refactor:`, `perf:`, `test:`, and others.
+    echo Do not add a footer. Do not add an introduction like 'The title should be...'. Just print the title and the body of the commit message without any other comment.
+    echo the title must not exceed 52 characters
+    echo the body and footer lines must not exceed 80 characters, and must not be indented, no prefix spaces.
+    echo If I provide an additional prompt explaining the context of this diff, do include that into your generated commit message.
+    echo Make sure the body includes two sections, Why and What.
+    echo In the 'why' section, do not use generic 'Improved xxx' without explaining why xxx is improved.
+    echo in the 'what' section, make a list of modifications, each line starting with a dash
+  ) > tmp.txt
+  %_ok% "Prompt from ghs.bat created."
+)
 echo Reminder: conventional commit means: the title must start with `^<type^>[optional scope]: description`, with 52 characters max>> tmp.txt
 echo Types other than `fix:` and `feat:` are `build:`, `chore:`, `ci:`, `docs:`, `style:`, `refactor:`, `perf:`, `test:`, and others.>> tmp.txt
 echo Do not add a footer. Do not add an introduction like 'The title should be...'. Just print the title and the body of the commit message without any other comment.>> tmp.txt
