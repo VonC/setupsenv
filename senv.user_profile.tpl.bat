70c96c9302ca6e8019e474ab3388a286a8634c2d
VonC <vonc@laposte.net>
2026-03-28 19:54:30 +0100

    fix(senv): Make sure _HOME_\bin\senv.bat runs from _HOME_
    Why:
    
    A user running the environment setup script from the `_HOME_` directory must not
    call the local senv, as it calls `%USERPROFILE%\senv.bat`, ... which calls
    `%HOME%\senv.bat`: recursive call.
    
    This change makes certain that when in _HOME_, the primary system environment
    setup script is used (the `%HOME%\bin\senv.bat` one).
    
    What:
    
    - Added a condition to check if the current directory is `_HOME_`.
    - If current directory is `_HOME_`, call `_HOME_\bin\senv.bat`.
    - Exit the script after sourcing `_HOME_\bin\senv.bat`.

--- senv.user_profile.tpl.bat
+++ senv.user_profile.tpl.bat
@@ -4,6 +4,10 @@ if not exist "%CD%\senv.bat" (
   call _HOME_\bin\senv.bat
   goto :eof
 )
+if "%CD%"=="_HOME_" (
+  call _HOME_\bin\senv.bat
+  goto :eof
+)
 if /I not "%~f0"=="%CD%\senv.bat" (
   call "%CD%\senv.bat"
   goto :eof
